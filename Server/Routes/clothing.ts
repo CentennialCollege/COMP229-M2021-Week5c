4d6597435e1d65e88fb3947414ba3f94e0ce554a
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2021-06-07 16:35:53 -0400

    added first draft of Edit page

--- Server/Routes/clothing.ts
+++ Server/Routes/clothing.ts
@@ -3,9 +3,10 @@ const router = express.Router();
 export default router;
 
 // create an clothing controller instance
-import { DisplayClothingListPage } from '../Controllers/clothing';
+import { DisplayClothingListPage, DisplayEditPage } from '../Controllers/clothing';
 
 /* GET clothing-list page - with /clothing-list */
 router.get('/', DisplayClothingListPage);
 
 /* display edit/:id page - with /clothing-list/edit:id */
+router.get('/edit/:id', DisplayEditPage);
